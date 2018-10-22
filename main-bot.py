256cfeef589c2b8eb6de55c6fe0ffac468bf85a1
Emilio Aburto L <emilio-aburto@users.noreply.github.com>
2018-10-22 14:24:32 -0300

    Agregar hashbang, y hacer que el main sea funcion

--- main-bot.py
+++ main-bot.py
@@ -1,13 +1,19 @@
+#! /usr/bin/env python3
 from telegram.ext import Updater, CommandHandler
 import mytoken
+
+
+def main():
+    updater = Updater(mytoken.TOKEN)
+    updater.dispatcher.add_handler(CommandHandler('hello', hello))
+    updater.start_polling()
+    updater.idle()
+
 
 def hello(bot, update):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
 
-updater = Updater(mytoken.TOKEN)
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
-
-updater.start_polling()
-updater.idle()
+if __name__ == "__main__":
+    main()
