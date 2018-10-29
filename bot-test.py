4e654478771bca81c022b521411c864ce9aa68be
Dario Andhael Palma <andhael@MendicantBias>
2018-10-29 15:39:42 -0300

    Se cambia el nombre del modulo main-bot, se anticipan test para puppy

--- bot-test.py
+++
@@ -1,7 +0,0 @@
-import requests
-import requests_mock
-
-def test_do():
-    with requests_mock.Mocker() as m:
-        m.get('http://test.com', text='resp')
-        assert requests.get('http://test.com').text == 'resp'
