2768dc9187dcc32c3c76c34eeed86f8175cf463d
Dario Andhael Palma <andhael@MendicantBias>
2018-10-29 14:32:15 -0300

    Se agrega un test de prueba funcional

--- bot-test.py
+++ bot-test.py
@@ -1,1 +1,7 @@
-assert 1
+import requests
+import requests_mock
+
+def test_do():
+    with requests_mock.Mocker() as m:
+        m.get('http://test.com', text='resp')
+        assert requests.get('http://test.com').text == 'resp'
